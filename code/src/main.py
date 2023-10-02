ab3660229d8db49da50f1c83bd3f0e8b2d93c7fd
Paul Bekaert <2517073B@student.gla.ac.uk>
2023-10-02 11:50:44 +0100

    added gitignore and experimental biostuff

--- code/src/main.py
+++ code/src/main.py
@@ -1,1 +1,17 @@
-import pytorch
+import os
+from Bio import SeqIO
+
+
+SEQ_FILE = 'data/Compara.110.ncrna_default.nt.fasta'
+
+sequences = list(SeqIO.parse(SEQ_FILE, "fasta"))
+
+
+if not sequences:
+	print("No sequences found.")
+else:
+	for seq_record in sequences:
+	    print(f"ID: {seq_record.id}")
+	    print(f"Description: {seq_record.description}")
+	    print(f"Sequence length: {len(seq_record.seq)}")
+	    print(f"Sequence: {seq_record.seq}\n")
