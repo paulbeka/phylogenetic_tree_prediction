9361d69dc340f71e8f309e63cb44abd382e89308
Paul Bekaert <2517073B@student.gla.ac.uk>
2023-10-22 18:20:03 +0100

    added a limited action space with a small action

--- code/src/main.py
+++ code/src/main.py
@@ -2,7 +2,11 @@ from sequence_loader import get_tree_and_alignment, get_alignment_sequence_dict
 import torch
 import dendropy
 from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor, Scorer
-from dendropy.calculate import treecompare
+from dendropy.calculate import treecompare#
+from Bio.Phylo import write, read
+
+
+actions = []
 
 
 def main():
@@ -11,17 +15,36 @@ def main():
 	
 	calculator = DistanceCalculator('identity')
 	distMatrix = calculator.get_distance(alignment)
-
 	treeConstructor = DistanceTreeConstructor()
-	upgma_tree = treeConstructor.upgma(distMatrix)
 
-	# classBio.Phylo.TreeConstruction.Scorer
-	# maybe this function can be used to score the difference?
-	# no, we need to turn the tree into a dendropy tree so we can then score it using their fancy techniques.
+	tree1 = read("data/fast_tree_dataset/COG527.sim.trim.tree", "newick")
+	tree2 = treeConstructor.upgma(distMatrix)
 
 	scorer = Scorer()
-	print(dir(scorer))
-	print(scorer.get_score(upgma_tree, alignment))
+	# print(scorer.get_score(tree1, tree2))
+
+	traverse_tree(tree2.root)
+	find_action_space(tree2)
+
+
+def find_action_space(tree):
+	nodes = []
+	for action in actions:
+		if str(action)[0] == "N":
+			nodes.append(action)
+
+	print(nodes)
+	common_ancestor = tree.common_ancestor(nodes[0], nodes[1])
+	child1, child2 = common_ancestor.clades
+	common_ancestor.clades = [child2, child1]
+
+
+def traverse_tree(clade, depth=0):
+	global actions
+	for child in clade.clades:
+		actions.append(child)
+		traverse_tree(child, depth + 1)
+
 
 
 if __name__ == "__main__":
