3f10c4826cdf6b8bb367deca27d35025e3a85a14
Paul Bekaert <2517073B@student.gla.ac.uk>
2024-01-15 09:02:38 +0000

    added req

--- code/src/test.py
+++ code/src/test.py
@@ -3,6 +3,8 @@ from tree_manager import Tree
 
 from subprocess import Popen
 from tqdm import tqdm
+import random
+import dendropy
 
 
 RAXML_NG_SCRIPT = "raxml-ng"
@@ -10,12 +12,13 @@ RAXML_NG_SCRIPT = "raxml-ng"
 
 def main():
 	tree = Tree()
+	create_dataset(tree, 10)
 	
 
 
 def calculate_raxml(tree):
 	data = dendropy.DnaCharacterMatrix.get(
-	    path="pythonidae.nex",
+	    path="pythonidae.nex",	    
 	    schema="nexus")
 	rx = raxml.RaxmlRunner()
 	tree = rx.estimate_tree(
@@ -26,11 +29,11 @@ def calculate_raxml(tree):
 
 # Or maybe I should find the optimal branch, and only train with that
 # it would be more accurate but the training time would be much longer
-def create_dataset(tree):
+def create_dataset(tree, n_items):
 	dataset = []
-	for i in range(1):
+	for i in range(n_items):
 		actionSpace = tree.find_action_space()
-		action = random.choice(possible_actions)
+		action = random.choice(actionSpace)
 		currentTree = tree.perform_spr(action[0], action[1])
 		treeProperties = get_tree_features(tree)
 		score = calculate_raxml(treeProperties)
