0b313e3dc6f26d812728102d0a51ae6f06f50b0e
Paul Bekaert <2517073B@student.gla.ac.uk>
2024-01-15 16:39:42 +0000

    added utils

--- code/src/main.py
+++ code/src/main.py
@@ -1,233 +1,40 @@
-from sequence_loader import get_tree_and_alignment, get_alignment_sequence_dict
-import torch
-import torch.nn as nn
-import dendropy
-from Bio.Phylo.TreeConstruction import DistanceCalculator, DistanceTreeConstructor, Scorer
-from dendropy.calculate import treecompare
-from Bio.Phylo import write, read
-from Bio import Phylo
-import copy
-import random
-from networks import neural_network
+from get_tree_features import get_tree_features
+from tree_manager import Tree
+from util.raxml_util import calculate_raxml
+
 from tqdm import tqdm
-import matplotlib.pyplot as plt
-from torch_geometric.nn import GCNConv
-from dendropy.interop import raxml
-import networkx as nx
-import torch.nn.functional as F
-from torch_geometric.data import Data
-from torch_geometric.utils import from_networkx
+import random
+import dendropy
 
 
-actions = []
+RAXML_NG_SCRIPT = "raxml-ng"
 
 
 def main():
-
-	_, alignment = get_tree_and_alignment("data/fast_tree_dataset/COG527")
-	
-	calculator = DistanceCalculator('identity')
-	distMatrix = calculator.get_distance(alignment)
-	treeConstructor = DistanceTreeConstructor()
-
-	original_tree = read("data/fast_tree_dataset/COG527.sim.trim.tree", "newick")
-	tree = treeConstructor.upgma(distMatrix)
+	tree = Tree()
+	create_dataset(tree, 1)
 
 
-	# LOAD THE DATA
-	n_train, n_test = 1000, 200
-
-	print("Loading training dataset...")
-	train_dataset = generateDataset(original_tree, n_train)
-	train_loader = torch.utils.data.DataLoader(
-		dataset=train_dataset, 
-		batch_size=1, 
-		shuffle=True)
-
-	displayMatplotlib(train_dataset)
-
-	print("Loading test dataset...")
-	test_dataset = generateDataset(original_tree, n_test)
-	test_loader = torch.utils.data.DataLoader(
-		dataset=test_dataset,
-		batch_size=1, 
-		shuffle=True
-	)
-	
-	# TRAIN THE NEURAL NETWORK
-
-	num_epochs = 10
-	batch_size = 1
-	learning_rate = 0.0001
-
-	model =  neural_network.ScoreFinder(batch_size)
-
-	criterion = nn.MSELoss()
-	optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
-
-	total_steps = len(train_loader)
-	for epoch in range(num_epochs):
-		print(f"Epoch: {epoch+1}/{num_epochs}")
-		for i, (configs, labels) in tqdm(enumerate(train_loader), desc="Training: ", total=len(train_loader)):
-			outputs = model(configs)
-			loss = criterion(outputs, labels)
-
-			optimizer.zero_grad()
-			loss.backward()
-			optimizer.step()
-
-
-	with torch.no_grad():
-		correct, samples = 0, 0
-		test_loss = 0
-		out = []
-		for configs, labels in test_loader:
-			ouputs = model(configs)
-			out.append(outputs)
-
-			# _, predictions = torch.max(outputs, 1)
-			test_loss += criterion(outputs, labels)
-
-		print(f"Total loss: {test_loss/len(test_loader)}")
-
-		print(out)
-
-
-def find_action_space(tree):
-	nodes = []
-
-	for clade in tree.find_clades():
-		nodes.append(clade)
-
-	actionSpace = []
-	for node in nodes:
-		for item in nodes:
-			if node == item:
-				break
-
-			actionSpace.append((node, item))
-
-	return actionSpace
-
-
-def perform_spr(tree, subtree, regraft_location):
-	parent = get_parent(tree, subtree)
-
-	if parent is None:
-		raise ValueError("Can't prune the root.")
-    
-	parent.clades.remove(subtree)
-    
-	if regraft_location:
-		regraft_location.clades.append(subtree)
-	else:
-		tree.root.clades.append(subtree)
-    
-	return tree
-
-
-def getTreeScore(tree, originalTree):
-	# likelihood score ? BioPython library might have it
-	treeProps = getTreeProperties(tree)
-	originalTreeProps = getTreeProperties(originalTree)
-
-	length_diff = abs(treeProps["total_branch_length"] - originalTreeProps["total_branch_length"])
-	max_branch_length_diff = abs(treeProps["max_branch_length"] - originalTreeProps["max_branch_length"])
-	avg_terminal_distance_diff = abs(treeProps["avg_terminal_distance"] - originalTreeProps["avg_terminal_distance"])
-	
-	return (avg_terminal_distance_diff * 10) + (max_branch_length_diff * 0.1)
-
-
-def getTreeProperties(tree):
-	properties = {}
-	properties['total_branch_length'] = tree.total_branch_length()
-
-	max_length = 0
-	n_terminals = 0
-	total_distance = 0
-	for element in tree.find_elements():
-		if element.is_terminal():
-			total_distance += tree.distance(element)
-			n_terminals += 1
-		if hasattr(element, "branch_length"):
-			if element.branch_length > max_length:
-				max_length = element.branch_length
-	
-	properties['max_branch_length'] = max_length
-	properties['avg_terminal_distance'] = total_distance / n_terminals
-
-	return properties
-
-
-# TODO: check that the generation is not just re-generating the same tree again and again. !!!
-def generateDataset(originalTree, n_items):
+# Or maybe I should find the optimal branch, and only train with that
+# it would be more accurate but the training time would be much longer
+def create_dataset(tree, n_items):
 	dataset = []
-	currentTree = copy.deepcopy(originalTree)
-	for i in tqdm(range(n_items)):
-		possible_actions = find_action_space(currentTree)
-		action = random.choice(possible_actions)
-		currentTree = copy.deepcopy(perform_spr(currentTree, action[0], action[1]))
-		inputMatrix = list(getTreeProperties(currentTree).values())
-		dataset.append((torch.FloatTensor(inputMatrix), torch.FloatTensor([getTreeScore(currentTree, originalTree)])))
+	for i in range(n_items):
+		actionSpace = tree.find_action_space()
+		action = random.choice(actionSpace)
+		# TODO: SPR operation not working --> not being parsed by RAxML-ng
+		tree.perform_spr(action[0], action[1])
+		treeProperties = get_tree_features(tree)
+		score = calculate_raxml(tree)
+		dataset.append((treeProperties, score))
 
 	return dataset
 
 
-def displayMatplotlib(dataset):
-	dat = [x[1] for x in dataset]
-	x_axis = list(range(len(dataset)))
-	
-	plt.plot(x_axis, dat)
-	plt.show()
-
-
-# UTILITY - GET PARENT OF A CLADE
-def get_parent(tree, child_clade):
-	node_path = tree.get_path(child_clade)
-	try:
-		return node_path[-2]
-	except:
-		return tree.root
+# use the dataset to train the value network
+def train_value_network():
+	pass
 
 
 if __name__ == "__main__":
 	main()
-
-
-
-	# This is RAxML for DNA
-	# data = dendropy.DnaCharacterMatrix.get(
-	#     path="pythonidae.nex",
-	#     schema="nexus")
-	# rx = raxml.RaxmlRunner()
-	# tree = rx.estimate_tree(
-	#         char_matrix=data,
-	#         raxml_args=["--no-bfgs"])
-	# print(tree.as_string(schema="newick"))
-
-
-	# graph = Phylo.to_networkx(tree)
-
-	# data = from_networkx(graph)
-
-	# class GNN(torch.nn.Module):
-	#     def __init__(self, input_dim, hidden_dim, output_dim):
-	#         super(GNN, self).__init__()
-	#         self.conv1 = GCNConv(input_dim, hidden_dim)
-	#         self.conv2 = GCNConv(hidden_dim, output_dim)
-
-	#     def forward(self, data):
-	#     	# Here, extract a meaninful feature to do something with it
-	#         x, edge_index = data.weight, data.edge_index
-	#         x = F.relu(self.conv1(x, edge_index))
-	#         x = F.relu(self.conv2(x, edge_index))
-	#         return F.log_softmax(x, dim=1)
-
-	# input_dim = 1
-	# hidden_dim = 64
-	# output_dim = 2
-
-	# model = GNN(input_dim, hidden_dim, output_dim)
-
-	# output = model(data)
-	# print(output)
---
+++ code/src/util/raxml_util.py
@@ -0,0 +1,75 @@
+from subprocess import Popen, PIPE, STDOUT
+import re
+
+
+def calculate_raxml(tree):
+	msa_file = "./"
+	tree_rampath = "/dev/shm/" + str(random.random())  + str(random.random()) + "tree"  # the var is the str: tmp{dir_suffix}
+
+	try:
+		with open(tree_rampath, "w") as fpw:
+			fpw.write(tree.tree.format("newick"))
+
+		raxmlProcess = Popen([RAXML_NG_SCRIPT, '--evaluate', '--msa', "data/fast_tree_dataset/COG527.fasta", '--opt-branches', 'on', '--opt-model', 'off', '--model', "LG", '--nofiles', '--tree', tree_rampath], 
+			stdout=PIPE, stdin=PIPE, stderr=STDOUT)
+
+		raxml_stdout = raxmlProcess.communicate()[0]
+		raxml_output = raxml_stdout.decode()
+
+		result = parse_raxmlNG_content(raxml_output) # for testing
+		print(result)
+		return result
+
+	except Exception as e:
+		print(e)
+
+
+def parse_raxmlNG_content(content):
+	"""
+	:return: dictionary with the attributes - string typed. if parameter was not estimated, empty string
+	"""
+	res_dict = dict.fromkeys(["ll", "pInv", "gamma",
+							  "fA", "fC", "fG", "fT",
+							  "subAC", "subAG", "subAT", "subCG", "subCT", "subGT",
+							  "time"], "")
+
+	# likelihood
+	ll_re = re.search("Final LogLikelihood:\s+(.*)", content)
+	if ll_re:
+		res_dict["ll"] = ll_re.group(1).strip()
+	elif re.search("BL opt converged to a worse likelihood score by", content) or re.search("failed", content):   # temp, till next version is available
+		ll_ini = re.search("initial LogLikelihood:\s+(.*)", content)
+		if ll_ini:
+			res_dict["ll"] = ll_ini.group(1).strip()
+	else:
+		res_dict["ll"] = 'unknown raxml-ng error, check "parse_raxmlNG_content" function'
+
+
+	# gamma (alpha parameter) and proportion of invariant sites
+	gamma_regex = re.search("alpha:\s+(\d+\.?\d*)\s+", content)
+	pinv_regex = re.search("P-inv.*:\s+(\d+\.?\d*)", content)
+	if gamma_regex:
+		res_dict['gamma'] = gamma_regex.group(1).strip()
+	if pinv_regex:
+		res_dict['pInv'] = pinv_regex.group(1).strip()
+
+	# Nucleotides frequencies
+	nucs_freq = re.search("Base frequencies.*?:\s+(\d+\.?\d*)\s+(\d+\.?\d*)\s+(\d+\.?\d*)\s+(\d+\.?\d*)", content)
+	if nucs_freq:
+		for i,nuc in enumerate("ACGT"):
+			res_dict["f" + nuc] = nucs_freq.group(i+1).strip()
+
+	# substitution frequencies
+	subs_freq = re.search("Substitution rates.*:\s+(\d+\.?\d*)\s+(\d+\.?\d*)\s+(\d+\.?\d*)\s+(\d+\.?\d*)\s+(\d+\.?\d*)\s+(\d+\.?\d*)", content)
+	if subs_freq:
+		for i,nuc_pair in enumerate(["AC", "AG", "AT", "CG", "CT", "GT"]):  # todo: make sure order
+			res_dict["sub" + nuc_pair] = subs_freq.group(i+1).strip()
+
+	# Elapsed time of raxml-ng optimization
+	rtime = re.search("Elapsed time:\s+(\d+\.?\d*)\s+seconds", content)
+	if rtime:
+		res_dict["time"] = rtime.group(1).strip()
+	else:
+		res_dict["time"] = 'no ll opt_no time'
+
+	return res_dict
